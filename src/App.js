cf9ab4e81857feabe63f8f656dfb3f010c02a491
Ritik Chauhan <ritik.chauhan@quokkalabs.com>
2022-10-31 17:22:19 +0530

    lazy routing implemented

--- src/App.js
+++ src/App.js
@@ -2,13 +2,37 @@ import logo from "./logo.svg";
 import "./App.css";
 import Component2 from "./components/Component2";
 import { Component1 } from "./components/Component1";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { Suspense } from "react";
+// import Home from "./lazyFolder/Home";
+// import Card from "./lazyFolder/Card";
+const Home = React.lazy(() => import("./lazyFolder/Home"));
+const Card = React.lazy(() => import("./lazyFolder/Card"));
 
 function App() {
   return (
     <div className="App">
-      <Component1>
-        <Component2/>
-      </Component1>
+      <Router>
+        <Routes>
+          <Route
+            path="home"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <Home />
+              </Suspense>
+            }
+          />
+          <Route path="card" element={<Card />} />
+          <Route
+            path="/"
+            element={
+              <Component1>
+                <Component2 />
+              </Component1>
+            }
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/Component1.js
+++ src/components/Component1.js
@@ -1,25 +1,30 @@
 import React, { useContext } from "react";
 import { createContext, useState } from "react";
+import { Link } from "react-router-dom";
 // import Component2 from "./Component2";
 
 const ComponentContext = createContext();
 
-export const Component1 = ({children}) => {
-
+export const Component1 = ({ children }) => {
   const [user, setUser] = useState("Google Stark");
-//   const valueObject={
-//     user123:user
-// }
+  //   const valueObject={
+  //     user123:user
+  // }
   return (
-   
-
-    //   <ComponentContext.Provider value={valueObject}>
-    <ComponentContext.Provider value={{user}}>
+    <>
+      <div>Main Component COMPONENT-1 which wraps all component</div>
+      <div className="d-flex gap-3 align-items-center">
+        <h4><Link to={"home"}>Home</Link></h4>
+        <Link to={"card"}>Card</Link>
+      </div>
+      {/* <ComponentContext.Provider value={valueObject}> */}
+      <ComponentContext.Provider value={{ user }}>
         {children}
       </ComponentContext.Provider>
+    </>
   );
 };
 
 export const UseUserAuthContext = () => {
-    return useContext(ComponentContext);
-  };
+  return useContext(ComponentContext);
+};
